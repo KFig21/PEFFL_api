a06fc2851acef1979a194a8b13211f59b67cbce2
Kevin <kfig21@gmail.com>
2022-04-12 18:16:35 -0400

    update port - getting CORS error

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-// const port = 3001;
+// const port = 3000;
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}!`);
 // });
