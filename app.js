4ab35d20641374bda9a4f8bdcb9805c21f85c623
Kevin <kfig21@gmail.com>
2022-04-12 22:25:36 -0400

    update port - getting CORS error

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ const corsOptions = {
   origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
+  origin: true,
 };
 app.use(cors(corsOptions)); // Use this after the variable declaration
 app.options("*", cors());
