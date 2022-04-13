57c58ad7e6c6b7a9c04ada7c38a343e858797706
Kevin <kfig21@gmail.com>
2022-04-12 22:18:04 -0400

    update port - getting CORS error

--- app.js
+++ app.js
@@ -9,17 +9,6 @@ const bodyParser = require("body-parser");
 const createError = require("http-errors");
 const path = require("path");
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requrested-With, Content-Type, Accept, content-type"
-  );
-  next();
-});
-
 // import routes
 const authRouter = require("./routes/auth");
 const regularSeasonRouter = require("./routes/regularSeason");
