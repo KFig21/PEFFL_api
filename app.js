47331b1dd24fbf630b80a456740eeb361d60b9e9
KFig21 <kfig21@gmail.com>
2023-08-11 22:45:50 -0400

    cors

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
   next();
 })
 app.options("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://example.com");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.sendStatus(204);
